0df4978d064ef3c302c8ecb61a20b7934371a9a7
Bob Orchard <mail@boborchard.com>
2020-11-13 14:45:44 -0600

    Move all movies to a mock API

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,43 +1,32 @@
+import React from 'react';
+import axios from 'axios';
 import ListItem from './ListItem'
 
-const movies = [
-{
-  name: "Death Becomes Her",
-  category: "Comedy",
-  rating: "3",
-},
-{
-  name: "Ghostbusters",
-  category: "Comedy",
-  rating: "5",
-  // avatar: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/di9t2YVpWDKuUtYkrm10yVld50x.jpg"
-},
-{
-  name: "Harry Potter - Sorcerer's Stone",
-  category: "Drama",
-  rating: "4",
-},
-{
-  name: "Jurassic Park",
-  category: "Action/Adventure",
-  rating: "4",
-  // avatar: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/9i3plLl89DHMz7mahksDaAo7HIS.jpg"
-},
-{
-  name: "The Sandlot",
-  category: "Comedy",
-  rating: "4",
-  // avatar: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/dQkE6saKpkBxKsuGG2bU3CXQwMX.jpg"
-}]
+class List extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      data: []
+    }
+    this.apiUrl = 'https://5fadd4f12ec98b0016048b4a.mockapi.io/api/v1/movies'
+  }
 
-function List() {
-  return (
-    <section>
-      {movies.map((movie) => (
-          <ListItem {...movie} />
-        ))}
-    </section>
-  );
+  componentDidMount(){
+    axios.get(this.apiUrl)
+      .then((res) => {
+        this.setState({data:res.data});
+      });
+  }
+
+  render(){
+    return (
+      <section>
+        {this.state.data.map((movie) => (
+            <ListItem {...movie} />
+          ))}
+      </section>
+    );
+  }
 }
 
 export default List;
