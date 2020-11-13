178c9b99b988117100b60c0b8fceedfdf0ba017f
Bob Orchard <mail@boborchard.com>
2020-11-13 10:27:21 -0600

    Stub out header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,23 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header.attrs({
+  className: "flex items-center p-10",
+})`
+  border-bottom: 1px solid #EEEEEE;
+  font-size: 40px;
+`;
+
+const StyledLogo = styled.h1.attrs({
+  className: "font-bold"
+})`
+  font-size: 2.5rem;
+`;
+
 function Header() {
   return (
-    <>
-      Header Component
-    </>
+    <StyledHeader>
+      <StyledLogo>Screen it</StyledLogo>
+    </StyledHeader>
   );
 }
 
