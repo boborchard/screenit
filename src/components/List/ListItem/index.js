0af2e220ffef78caa757bc30552c8e04545896f5
Bob Orchard <mail@boborchard.com>
2020-11-13 10:30:29 -0600

    Stub out list component

---
+++ src/components/List/ListItem/index.js
@@ -0,0 +1,38 @@
+import Avatar from 'react-avatar';
+import StarRating from 'react-svg-star-rating';
+
+function ListItem(movie) {
+  return (
+    <div className="flex items-center justify-items-stretch px-10 py-8 border-t border-gray w-full">
+      <Avatar
+        name={movie.name.replace(/[^a-zA-Z ]/g, "")}
+        src={movie.avatar}
+        round={true}
+        color="#61A0FF"
+        maxInitials={3}
+        size={64}
+      />
+      <div className="flex-shrink pl-6 w-3/5 pr-6">
+        <h2 className="font-medium text-2xl truncate w-full max-w-full">{movie.name}</h2>
+        <p className="text-brand-gray-dark">{movie.category}</p>
+      </div>
+      <div className="flex-1">
+        <StarRating
+          name="rating"
+          containerClassName="flex flex-row items-center w-full justify-end"
+          count={5}
+          initialRating={movie.rating}
+          isReadOnly="true"
+          emptyColor="#CCCCCC"
+          activeColor="#FFC93D"
+          hoverColor="#FFC93D"
+          outerRadius={45}
+          innerRadius={25}
+          size={32}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default ListItem;
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,8 +1,42 @@
+import ListItem from './ListItem'
+
+const movies = [
+{
+  name: "Death Becomes Her",
+  category: "Comedy",
+  rating: "3",
+},
+{
+  name: "Ghostbusters",
+  category: "Comedy",
+  rating: "5",
+  // avatar: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/di9t2YVpWDKuUtYkrm10yVld50x.jpg"
+},
+{
+  name: "Harry Potter - Sorcerer's Stone",
+  category: "Drama",
+  rating: "4",
+},
+{
+  name: "Jurassic Park",
+  category: "Action/Adventure",
+  rating: "4",
+  // avatar: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/9i3plLl89DHMz7mahksDaAo7HIS.jpg"
+},
+{
+  name: "The Sandlot",
+  category: "Comedy",
+  rating: "4",
+  // avatar: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/dQkE6saKpkBxKsuGG2bU3CXQwMX.jpg"
+}]
+
 function List() {
   return (
-    <>
-      List Component
-    </>
+    <div className="movies-list">
+      {movies.map((movie) => (
+          <ListItem {...movie} />
+        ))}
+    </div>
   );
 }
 
